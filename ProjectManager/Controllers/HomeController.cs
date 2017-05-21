5b01f4e0a32f87d479abc2271ef7c3588aaeb3cc
eli reid <Eli.R@emrelectrical.com>
2017-05-21 17:52:16 -0400

    more stuff

--- ProjectManager/Controllers/HomeController.cs
+++ ProjectManager/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace ProjectManager.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
